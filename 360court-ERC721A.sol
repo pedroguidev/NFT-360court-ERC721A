9e88e1245f097a43d4d2a00cf8c0ef3007c3b28e
pedroguidev <pgelevenocean@gmail.com>
2023-06-03 05:21:43 +0300

    fix uri issue

--- 360court-ERC721A.sol
+++ 360court-ERC721A.sol
@@ -460,8 +460,6 @@ interface IERC721Receiver {
 }
 
 
-
-
 /*
  * @dev Provides information about the current execution context, including the
  * sender of the transaction and its data. While these are generally available
@@ -577,7 +575,7 @@ contract ERC721A is
     uint128 numberMinted;
   }
 
-  uint256 private currentIndex = 0;
+  uint256 private currentIndex = 8740000;
 
   uint256 internal immutable collectionSize;
   uint256 internal immutable maxBatchSize;
@@ -760,6 +758,8 @@ contract ERC721A is
     );
 
     string memory baseURI = _baseURI();
+
+
     return
       bytes(baseURI).length > 0
         ? string(abi.encodePacked(baseURI, tokenId.toString()))
@@ -1222,8 +1222,6 @@ abstract contract Ownable is Context {
 
 contract Azuki is Ownable, ERC721A, ReentrancyGuard {
   uint256 public immutable maxPerAddressDuringMint;
-  uint256 public immutable amountForDevs;
-  uint256 public immutable amountForAuctionAndDev;
 
   struct SaleConfig {
     uint32 auctionSaleStartTime;
@@ -1235,168 +1233,41 @@ contract Azuki is Ownable, ERC721A, ReentrancyGuard {
 
   SaleConfig public saleConfig;
 
-  mapping(address => uint256) public allowlist;
-
   constructor(
     uint256 maxBatchSize_,
-    uint256 collectionSize_,
-    uint256 amountForAuctionAndDev_,
-    uint256 amountForDevs_
-  ) ERC721A("Azuki", "AZUKI", maxBatchSize_, collectionSize_) {
+    uint256 collectionSize_
+  ) ERC721A("Court 360", "C360", maxBatchSize_, collectionSize_) {
     maxPerAddressDuringMint = maxBatchSize_;
-    amountForAuctionAndDev = amountForAuctionAndDev_;
-    amountForDevs = amountForDevs_;
-    require(
-      amountForAuctionAndDev_ <= collectionSize_,
-      "larger collection size needed"
-    );
   }
 
-  modifier callerIsUser() {
-    require(tx.origin == msg.sender, "The caller is another contract");
-    _;
-  }
-
-  function auctionMint(uint256 quantity) external payable callerIsUser {
-    uint256 _saleStartTime = uint256(saleConfig.auctionSaleStartTime);
-    require(
-      _saleStartTime != 0 && block.timestamp >= _saleStartTime,
-      "sale has not started yet"
-    );
-    require(
-      totalSupply() + quantity <= amountForAuctionAndDev,
-      "not enough remaining reserved for auction to support desired mint amount"
-    );
-    require(
-      numberMinted(msg.sender) + quantity <= maxPerAddressDuringMint,
-      "can not mint this many"
-    );
-    uint256 totalCost = getAuctionPrice(_saleStartTime) * quantity;
-    _safeMint(msg.sender, quantity);
-    refundIfOver(totalCost);
-  }
-
-  function allowlistMint() external payable callerIsUser {
-    uint256 price = uint256(saleConfig.mintlistPrice);
-    require(price != 0, "allowlist sale has not begun yet");
-    require(allowlist[msg.sender] > 0, "not eligible for allowlist mint");
-    require(totalSupply() + 1 <= collectionSize, "reached max supply");
-    allowlist[msg.sender]--;
-    _safeMint(msg.sender, 1);
-    refundIfOver(price);
-  }
-
-  function publicSaleMint(uint256 quantity, uint256 callerPublicSaleKey)
-    external
-    payable
-    callerIsUser
-  {
-    SaleConfig memory config = saleConfig;
-    uint256 publicSaleKey = uint256(config.publicSaleKey);
-    uint256 publicPrice = uint256(config.publicPrice);
-    uint256 publicSaleStartTime = uint256(config.publicSaleStartTime);
-    require(
-      publicSaleKey == callerPublicSaleKey,
-      "called with incorrect public sale key"
-    );
-
-    require(
-      isPublicSaleOn(publicPrice, publicSaleKey, publicSaleStartTime),
-      "public sale has not begun yet"
-    );
-    require(totalSupply() + quantity <= collectionSize, "reached max supply");
-    require(
-      numberMinted(msg.sender) + quantity <= maxPerAddressDuringMint,
-      "can not mint this many"
-    );
-    _safeMint(msg.sender, quantity);
-    refundIfOver(publicPrice * quantity);
-  }
-
-  function refundIfOver(uint256 price) private {
-    require(msg.value >= price, "Need to send more ETH.");
-    if (msg.value > price) {
-      payable(msg.sender).transfer(msg.value - price);
-    }
-  }
-
-  function isPublicSaleOn(
-    uint256 publicPriceWei,
-    uint256 publicSaleKey,
-    uint256 publicSaleStartTime
-  ) public view returns (bool) {
-    return
-      publicPriceWei != 0 &&
-      publicSaleKey != 0 &&
-      block.timestamp >= publicSaleStartTime;
-  }
-
-  uint256 public constant AUCTION_START_PRICE = 1 ether;
-  uint256 public constant AUCTION_END_PRICE = 0.15 ether;
-  uint256 public constant AUCTION_PRICE_CURVE_LENGTH = 340 minutes;
-  uint256 public constant AUCTION_DROP_INTERVAL = 20 minutes;
-  uint256 public constant AUCTION_DROP_PER_STEP =
-    (AUCTION_START_PRICE - AUCTION_END_PRICE) /
-      (AUCTION_PRICE_CURVE_LENGTH / AUCTION_DROP_INTERVAL);
-
-  function getAuctionPrice(uint256 _saleStartTime)
+    function tokenURI(uint256 tokenId)
     public
     view
-    returns (uint256)
-  {
-    if (block.timestamp < _saleStartTime) {
-      return AUCTION_START_PRICE;
-    }
-    if (block.timestamp - _saleStartTime >= AUCTION_PRICE_CURVE_LENGTH) {
-      return AUCTION_END_PRICE;
-    } else {
-      uint256 steps = (block.timestamp - _saleStartTime) /
-        AUCTION_DROP_INTERVAL;
-      return AUCTION_START_PRICE - (steps * AUCTION_DROP_PER_STEP);
-    }
-  }
-
-  function endAuctionAndSetupNonAuctionSaleInfo(
-    uint64 mintlistPriceWei,
-    uint64 publicPriceWei,
-    uint32 publicSaleStartTime
-  ) external onlyOwner {
-    saleConfig = SaleConfig(
-      0,
-      publicSaleStartTime,
-      mintlistPriceWei,
-      publicPriceWei,
-      saleConfig.publicSaleKey
-    );
-  }
-
-  function setAuctionSaleStartTime(uint32 timestamp) external onlyOwner {
-    saleConfig.auctionSaleStartTime = timestamp;
-  }
-
-  function setPublicSaleKey(uint32 key) external onlyOwner {
-    saleConfig.publicSaleKey = key;
-  }
-
-  function seedAllowlist(address[] memory addresses, uint256[] memory numSlots)
-    external
-    onlyOwner
+    virtual
+    override
+    returns (string memory)
   {
     require(
-      addresses.length == numSlots.length,
-      "addresses does not match numSlots length"
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
     );
-    for (uint256 i = 0; i < addresses.length; i++) {
-      allowlist[addresses[i]] = numSlots[i];
-    }
+    string memory fullURI;
+    if (tokenId >= 8741000 && tokenId < 8741053)
+    fullURI = "https://lavender-grim-deer-416.mypinata.cloud/ipfs/QmRN511Rb2CSgJ1QPpJuqe7yPUpHa8y6nniWTSnASaVfBT/02pt.mp4";
+    if (tokenId >= 8741053 && tokenId < 8741103)
+    fullURI = "https://lavender-grim-deer-416.mypinata.cloud/ipfs/QmRN511Rb2CSgJ1QPpJuqe7yPUpHa8y6nniWTSnASaVfBT/03pt.mp4";
+    if (tokenId >= 8741103 && tokenId < 8741113)
+    fullURI = "https://lavender-grim-deer-416.mypinata.cloud/ipfs/QmRN511Rb2CSgJ1QPpJuqe7yPUpHa8y6nniWTSnASaVfBT/05pt.mp4";
+    if (tokenId >= 8741113 && tokenId < 8741123)
+    fullURI = "https://lavender-grim-deer-416.mypinata.cloud/ipfs/QmRN511Rb2CSgJ1QPpJuqe7yPUpHa8y6nniWTSnASaVfBT/10pt.mp4";
+    if (tokenId >= 8741123 && tokenId < 8741133)
+    fullURI = "https://lavender-grim-deer-416.mypinata.cloud/ipfs/QmRN511Rb2CSgJ1QPpJuqe7yPUpHa8y6nniWTSnASaVfBT/15pt.mp4";
+    if (tokenId >= 8741133 && tokenId < 8741143)
+    fullURI = "https://lavender-grim-deer-416.mypinata.cloud/ipfs/QmRN511Rb2CSgJ1QPpJuqe7yPUpHa8y6nniWTSnASaVfBT/20pt.mp4";
+    return    fullURI;
   }
 
-  // For marketing etc.
   function devMint(uint256 quantity) external onlyOwner {
-    require(
-      totalSupply() + quantity <= amountForDevs,
-      "too many already minted before dev mint"
-    );
     require(
       quantity % maxBatchSize == 0,
       "can only mint a multiple of the maxBatchSize"
@@ -1416,15 +1287,6 @@ contract Azuki is Ownable, ERC721A, ReentrancyGuard {
 
   function setBaseURI(string calldata baseURI) external onlyOwner {
     _baseTokenURI = baseURI;
-  }
-
-  function withdrawMoney() external onlyOwner nonReentrant {
-    (bool success, ) = msg.sender.call{value: address(this).balance}("");
-    require(success, "Transfer failed.");
-  }
-
-  function setOwnersExplicit(uint256 quantity) external onlyOwner nonReentrant {
-    _setOwnersExplicit(quantity);
   }
 
   function numberMinted(address owner) public view returns (uint256) {
